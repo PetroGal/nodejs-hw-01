1855de2fe713dd3c69f993e46e3a2b85f6fe460e
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-08-22 19:41:43 +0300

    initial commit

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import createFakeContact from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  const items = [];
+
+  for (let i = 0; i < number; i += 1) {
+    const contact = createFakeContact();
+    items.push(contact);
+  }
+  console.log(items);
+};
 
 generateContacts(5);
