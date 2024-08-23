166d6eed33ee407dc87f591aa3a8eaaa071db3db
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-08-23 12:39:29 +0300

    created generateContacts function and path to db

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,7 @@
-export const PATH_DB =
+import * as path from 'path';
+
+export const PATH_DB_CONTACTS = '';
+
+// const path1 = path.join(process.cwd(), 'src', 'db', 'db-contacts.json');
+const path2 = path.resolve('src', 'db', 'db-contacts.json');
+console.log(path2);
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -2,13 +2,8 @@ import { PATH_DB } from '../constants/contacts.js';
 import createFakeContact from '../utils/createFakeContact.js';
 
 const generateContacts = async (number) => {
-  const items = [];
-
-  for (let i = 0; i < number; i += 1) {
-    const contact = createFakeContact();
-    items.push(contact);
-  }
-  console.log(items);
+  const newContactList = Array(number).fill(0).map(createFakeContact);
+  console.log(newContactList);
 };
 
 generateContacts(5);
