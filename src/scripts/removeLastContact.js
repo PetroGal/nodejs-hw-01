62eb234d03b03d6995c9791f63c363b3dfaa0b01
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-08-24 17:15:20 +0300

    completed home work

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,9 @@
-// import { PATH_DB } from '../constants/contacts.js';
+import getAllContacts from './getAllContacts.js';
+import updateContacts from './updateContacts.js';
 
 export const removeLastContact = async () => {};
+const contactList = await getAllContacts();
+contactList.pop();
+await updateContacts(contactList);
 
 removeLastContact();
